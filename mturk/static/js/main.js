ec71bda1bf9a4ac0c8d126528d6c3aa6b1a6800e
Qiao Yan <alice.yan@hotmail.com>
2016-07-21 08:11:07 -0700

    style tag?

--- mturk/static/js/main.js
+++ mturk/static/js/main.js
@@ -117,15 +117,15 @@ function setupTag() {
 		for (var option in references[category]) {
 			// console.log(option);
 			// if (references[category][option].length != 0) {
-			if (!(option in tag_exp)) {
-				out+='<ul class=\"imagewrap\"><legend>'+option+': </legend>';
-			} else {
+			if ((option in tag_exp)) {
+				// out+='<ul class=\"imagewrap\"><legend>'+option+': </legend>';
+			// } else {
 				out+='<ul class=\"imagewrap\"><legend style="width:300px;">'+option+': '+tag_exp[option]+'</legend>';
 			}
 			
-			for (var reference in references[category][option]) {
-				out+='<li><img src=\"'+ references[category][option][reference] +'\"></li>';
-			}
+			// for (var reference in references[category][option]) {
+			// 	out+='<li><img src=\"'+ references[category][option][reference] +'\"></li>';
+			// }
 			out+='</ul>';
 			// }
 			// if (category == "ethnicity" ||category =="body-shape") {
