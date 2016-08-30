48b352fe84b7a86ea4c03bfd5652a9b0124badaa
Qiao Yan <alice.yan@hotmail.com>
2016-08-30 13:35:55 -0700

    rec tool

--- mturk/views.py
+++ mturk/views.py
@@ -1,58 +1,47 @@
 # from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
 from django.shortcuts import render_to_response
-from mturk.models import Task
 from django.views.decorators.csrf import csrf_exempt
-NUM = 10
-
-# AMAZON_HOST = "https://workersandbox.mturk.com/mturk/externalSubmit"
-# AMAZON_HOST = "https://www.mturk.com/mturk/externalSubmit"
+import json
+import subprocess
+# import shlex
+import ast
 
 @csrf_exempt
 def index(request):
-    print(request.GET.get("user-input", ""))
-    print(request.POST.get("user-input2", ""))
-    print(request.POST.get("imageIndex", ""))
-    # if request.GET.get("assignmentId") == "ASSIGNMENT_ID_NOT_AVAILABLE" or request.GET.get("assignmentId") =="":
-    #     render_data = {
-    #         "worker_id": request.GET.get("workerId", ""),
-    #         "assignment_id": request.GET.get("assignmentId", ""),
-    #         "amazon_host": AMAZON_HOST,
-    #         "hit_id": request.GET.get("hitId", ""),
-    #         "image_index": "0",
-    #     }
-    # else:
-    i = 0
-    a = Task.objects.all()
-    if (len(a) == 0):
-        i = 0
-    else:
-        i = a[len(a)-1].image_index+NUM
-    # record = Task.objects.filter(hit_id=request.GET.get("hitId", ""))
-    # if (len(record) == 0):
-    #     Task.objects.create(
-    #         hit_id=request.GET.get("hitId", ""),
-    #         image_index=i
-    #         )
-    #     imageIndex = i
-    # else:
-    #     imageIndex=record[0].image_index
-    if (request.GET.get("user-input", "") != ''):
-        Task.objects.create(hit_id = request.GET.get("user-input", ""),
-            image_index = i)
-        i+=NUM
-    render_data = {
-        # "worker_id": request.GET.get("workerId", ""),
-        # "assignment_id": request.GET.get("assignmentId", ""),
-        # "amazon_host": AMAZON_HOST,
-        # "hit_id": request.GET.get("hitId", ""),
-        "image_index": str(i),
-    }
-        
-
+    print("reach")
+    # c_line = 'curl -H "Content-Type: application/json" -d '{"key1":"v", "key2":"e", "key3":"t"}' https://wix5uh8vve.execute-api.us-west-2.amazonaws.com/prod/clueless/get-profile-by-id ; echo'
+    # commands = ['curl', '-i', '-H', 'Content-Type: application/json', '-d', '{"key1":"v", "key2":"e", "key3":"t"}', 'https://wix5uh8vve.execute-api.us-west-2.amazonaws.com/prod/clueless/get-profile-by-id', ';', 'echo']
+    # commands = ['curl', '-i', '-H', 'Content-Type: application/json', '-d', str(json.dumps(a)), 'https://wix5uh8vve.execute-api.us-west-2.amazonaws.com/prod/clueless/getoutfitimagesbykeyword', ';', 'echo']
+    # p = subprocess.Popen(commands, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    # out, err = p.communicate()
+    # print(out)
+    # print(err)
+    # print(dict(request.POST))
+    if (request.GET.get('userId','') != ''):
+        return display(request)
+    print("index")
+    render_data = {}
     response = render_to_response("index.html", render_data)
+    # without this header, your iFrame will not render in Amazon
+    response['x-frame-options'] = 'this_can_be_anything'
+    return response
+
+@csrf_exempt
+def display(request):
+    print("display")
+    commands = ['curl', '-H', 'Content-Type: application/json', '-d', '{"key1":"v", "key2":"e", "key3":"t"}', 'https://wix5uh8vve.execute-api.us-west-2.amazonaws.com/prod/clueless/get-profile-by-id']
+    # commands = ['curl', '-H', 'Content-Type: application/json', '-d', json.dumps(a), 'https://wix5uh8vve.execute-api.us-west-2.amazonaws.com/prod/clueless/getoutfitimagesbykeyword']
+    p = subprocess.Popen(commands, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    out, err = p.communicate()
+    print(out)
+    render_data = {
+        "age": age,
+        "skin-tone": skin_tone,
+        "body-type": body_type,
+    }
+    response = render_to_response("display.html", render_data)
     # without this header, your iFrame will not render in Amazon
     response['x-frame-options'] = 'this_can_be_anything'
     return response
