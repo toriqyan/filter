2260dbf013432aabff438ace99e30b1b862c42b8
Qiao Yan <alice.yan@hotmail.com>
2016-10-06 18:22:20 -0700

    new experiment

--- mturk/models.py
+++ mturk/models.py
@@ -5,8 +5,4 @@ from django.db import models
 # Create your models here.
 
 class Task(models.Model):
-	occasion = models.IntegerField(default=0)
-	season = models.IntegerField(default=0)
-	style = models.IntegerField(default=0)
-	ethnicity = models.IntegerField(default=0)
-	body_shape = models.IntegerField(default=0)
+	result = models.CharField(max_length = 10000)
